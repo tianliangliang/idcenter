2cd06278cadd401ab66f35ba361a08cab1ff91cf
tianliangliang <tianliangliang@mobike.com>
2018-03-16 19:35:38 +0800

    add annotate

--- src/main/java/com/sohu/idcenter/IdWorker.java
+++ src/main/java/com/sohu/idcenter/IdWorker.java
@@ -5,18 +5,41 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <br>
+ * SnowFlake的结构如下(每部分用-分开):
+ * <br>
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ * <br>
+ * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
+ * <br>
+ * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
+ * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
+ * <br>
+ * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
+ * <br>
+ * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
+ * <br>
+ * 加起来刚好64位，为一个Long型。
+ * <br>
+ * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
+ * <p>
+ * IdWorker{maxWorkerId=31, maxDatacenterId=31, sequenceMask=4095}
+ * <p>
  */
 
 package com.sohu.idcenter;
-
 
 import java.util.Random;
 
@@ -28,23 +51,40 @@ import java.util.Random;
  */
 public class IdWorker {
 
+    //工作机器ID(0~31)
     private final long workerId;
+    //数据中心ID(0~31)
     private final long datacenterId;
+
+    //时间起始标记点，作为基准，一般取系统的最近时间（一旦确定不能变动），默认：1344322705519L，2012-08-07 14:58:25
     private final long idepoch;
 
+    //机器标识位数
     private static final long workerIdBits = 5L;
+    //数据中心标识位数
     private static final long datacenterIdBits = 5L;
+    //机器ID最大值，结果是31 (这个移位算法可以很快的计算出几位二进制数所能表示的最大十进制数)
     private static final long maxWorkerId = -1L ^ (-1L << workerIdBits);
+    //数据中心ID最大值，结果是31
     private static final long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
 
+    //毫秒内自增位
     private static final long sequenceBits = 12L;
+    //机器ID偏左移12位
     private static final long workerIdShift = sequenceBits;
+    //数据中心ID左移17位
     private static final long datacenterIdShift = sequenceBits + workerIdBits;
+    //时间毫秒左移22位
     private static final long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+
+    //生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
     private static final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
+    //上次生产id时间戳
     private long lastTimestamp = -1L;
+    //毫秒内序列(0~4095)
     private long sequence;
+
     private static final Random r = new Random();
 
     public IdWorker() {
@@ -93,20 +133,34 @@ public class IdWorker {
         return id;
     }
 
+
+    /**
+     * 获得下一个ID (该方法是线程安全的)
+     *
+     * @return
+     */
     private synchronized long nextId() {
         long timestamp = timeGen();
+        //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
         if (timestamp < lastTimestamp) {
             throw new IllegalStateException("Clock moved backwards.");
         }
+
+        //如果是同一时间生成的，则进行毫秒内序列
         if (lastTimestamp == timestamp) {
+            // 当前毫秒内，则+1
             sequence = (sequence + 1) & sequenceMask;
             if (sequence == 0) {
+                // 当前毫秒内计数满了，阻塞到下一个毫秒
                 timestamp = tilNextMillis(lastTimestamp);
             }
         } else {
+            //时间戳改变，毫秒内序列重置
             sequence = 0;
         }
+        //上次生成ID的时间截
         lastTimestamp = timestamp;
+        //移位并通过或运算拼到一起组成64位的ID
         long id = ((timestamp - idepoch) << timestampLeftShift)//
                 | (datacenterId << datacenterIdShift)//
                 | (workerId << workerIdShift)//
@@ -116,13 +170,20 @@ public class IdWorker {
 
     /**
      * get the timestamp (millis second) of id
+     *
      * @param id the nextId
      * @return the timestamp of id
      */
-    public long getIdTimestamp(long id){
+    public long getIdTimestamp(long id) {
         return idepoch + (id >> timestampLeftShift);
     }
 
+    /**
+     * 阻塞到下一个毫秒，直到获得新的时间戳
+     *
+     * @param lastTimestamp 上次生成ID的时间截
+     * @return 当前时间戳
+     */
     private long tilNextMillis(long lastTimestamp) {
         long timestamp = timeGen();
         while (timestamp <= lastTimestamp) {
@@ -131,8 +192,22 @@ public class IdWorker {
         return timestamp;
     }
 
+    /**
+     * 返回以毫秒为单位的当前时间
+     *
+     * @return 当前时间(毫秒)
+     */
     private long timeGen() {
         return System.currentTimeMillis();
+    }
+
+    public String getMaxData() {
+        final StringBuilder sb = new StringBuilder("IdWorker{");
+        sb.append("maxWorkerId=").append(maxWorkerId);
+        sb.append(", maxDatacenterId=").append(maxDatacenterId);
+        sb.append(", sequenceMask=").append(sequenceMask);
+        sb.append('}');
+        return sb.toString();
     }
 
     @Override
